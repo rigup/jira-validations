99720672e164b0aab0e4998e6e3bf9296a842755
duanebester <bester.duane@gmail.com>
2019-09-23 14:51:32 -0700

    test branch

--- action.js
+++ action.js
@@ -19,6 +19,8 @@ module.exports = class {
   }
 
   validateTitleHasIssueId() {
+    if (this.eventName === "push") return true;
+
     return this.validateStringHasIssueId(this.githubEvent.pull_request.title);
   }
 
