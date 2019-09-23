f7f557fef8ccc498b994a941de515c0937008413
duanebester <bester.duane@gmail.com>
2019-09-23 09:14:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Jira Validations JavaScript Action
+
+This action prints verfied 'true' or 'false' if the github event contains a valid Jira Issue ID
+
+## Inputs
+
+### `verify-from`
+
+**Required** Can be `'branch'`, `'commits'` or `'all'`. Default `'branch'`.
+
+### `fail-invalid`
+
+**Required** Can be `'true'`, `'false'` or `'checks'`. Default `'checks'`.
+
+`'checks'` Will send a successful / failed github check
+
+## Outputs
+
+### `verified`
+
+If Jira Issue ID is valid
+
+## Example usage
+
+```
+uses: actions/jira-validations@v1
+with:
+  verify-from: 'branch'
+  fail-invalid: 'true'
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const jira = require("./common/net/jira");
+
+try {
+  if (!process.env.JIRA_BASE_URL)
+    throw new Error("Please specify JIRA_BASE_URL env");
+  if (!process.env.JIRA_API_TOKEN)
+    throw new Error("Please specify JIRA_API_TOKEN env");
+  if (!process.env.JIRA_USER_EMAIL)
+    throw new Error("Please specify JIRA_USER_EMAIL env");
+
+  // `verify-from` input defined in action metadata file
+  const verifyFrom = core.getInput("verify-from");
+  console.log(`Verifying Issue ID from ${verifyFrom}`);
+
+  // `verify-from` input defined in action metadata file
+  const verifyFrom = core.getInput("verify-from");
+  console.log(`Verifying Issue ID from ${verifyFrom}`);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+  const config = {
+    baseUrl: process.env.JIRA_BASE_URL,
+    token: process.env.JIRA_API_TOKEN,
+    email: process.env.JIRA_USER_EMAIL
+  };
+
+  this.jira = new jira(config);
+
+  core.setOutput("verified", true);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
