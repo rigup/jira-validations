062d2fe4fc1c594fb00444bb08094891c3f4dc06
duanebester <bester.duane@gmail.com>
2019-09-24 16:02:59 -0700

    Add aws sdk

--- dynamo.js
+++ dynamo.js
@@ -10,7 +10,7 @@ module.exports = class {
   }
 
   async findByGithubId(githubId) {
-    return findBy("githubId", githubId);
+    return await this.findBy("githubId", githubId);
   }
 
   async findBy(attr, value) {
