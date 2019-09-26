7c62b60fc95977f26aa5e7821dac93642faa0b78
duanebester <bester.duane@gmail.com>
2019-09-26 13:06:30 -0700

    fix master

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const Dynamo = require("./lib/dynamo");
-const Action = require("./lib/action");
-const Jira = require("./lib/jira");
+const core = require('@actions/core');
+const github = require('@actions/github');
+const Dynamo = require('./lib/dynamo');
+const Action = require('./lib/action');
+const Jira = require('./lib/jira');
 
 (async () => {
   try {
-    if (!process.env.JIRA_BASE_URL)
-      throw new Error("Please specify JIRA_BASE_URL env");
-    if (!process.env.JIRA_API_TOKEN)
-      throw new Error("Please specify JIRA_API_TOKEN env");
-    if (!process.env.JIRA_USER_EMAIL)
-      throw new Error("Please specify JIRA_USER_EMAIL env");
-    if (!process.env.PEOPLE_TABLE_NAME)
-      throw new Error("Please specify PEOPLE_TABLE_NAME env");
-    if (!process.env.GITHUB_TOKEN)
-      throw new Error("Please specify GITHUB_TOKEN env");
+    if (!process.env.JIRA_BASE_URL) throw new Error('Please specify JIRA_BASE_URL env');
+    if (!process.env.JIRA_API_TOKEN) throw new Error('Please specify JIRA_API_TOKEN env');
+    if (!process.env.JIRA_USER_EMAIL) throw new Error('Please specify JIRA_USER_EMAIL env');
+    if (!process.env.PEOPLE_TABLE_NAME) throw new Error('Please specify PEOPLE_TABLE_NAME env');
+    if (!process.env.GITHUB_TOKEN) throw new Error('Please specify GITHUB_TOKEN env');
 
     // `verify-from` input defined in action.yml
-    const verifyFromInput = core.getInput("verify-from");
+    const verifyFromInput = core.getInput('verify-from');
     console.log(`Verifying Issue ID from '${verifyFromInput}'`);
 
     // `fail-invalid` input defined in action.yml
-    const failInvalidInput = core.getInput("fail-invalid");
+    const failInvalidInput = core.getInput('fail-invalid');
     console.log(`Fail Invalid? ${failInvalidInput}`);
 
     // `allowed-issue-types` input defined in action.yml
-    const allowedIssueTypesInput = core
-      .getInput("allowed-issue-types")
-      .split(",");
-    console.log(
-      `Allowed Issue Types - ${JSON.stringify(allowedIssueTypesInput)}`
-    );
+    const allowedIssueTypesInput = core.getInput('allowed-issue-types').split(',');
+    console.log(`Allowed Issue Types - ${JSON.stringify(allowedIssueTypesInput)}`);
 
     const config = {
       baseUrl: process.env.JIRA_BASE_URL,
       token: process.env.JIRA_API_TOKEN,
-      email: process.env.JIRA_USER_EMAIL
+      email: process.env.JIRA_USER_EMAIL,
     };
 
     const jira = new Jira(config);
@@ -46,35 +37,32 @@ const Jira = require("./lib/jira");
     const { context } = github;
     const action = new Action({ context, jira, octokit });
 
-    const valid = await action.validate(
-      verifyFromInput,
-      allowedIssueTypesInput
-    );
+    const valid = await action.validate(verifyFromInput, allowedIssueTypesInput);
 
-    if (!valid && failInvalidInput === "true") {
-      core.setFailed("Validation Failed!");
+    if (!valid && failInvalidInput === 'true') {
+      core.setFailed('Validation Failed!');
     }
 
     const reviewers = action.getCodeReviewers();
     const rigupReviewers = await Promise.all(
-      reviewers.map(async reviewer => {
+      reviewers.map(async (reviewer) => {
         return dynamo.findByGithubId(reviewer.id);
       })
     );
 
-    const jiraAccountIds = rigupReviewers.map(rev => rev.Items[0].bitbucketId);
-    const jiraUsers = await jira.getUsersFromAccountIds(jiraAccountIds);
+    const jiraAccountIds = rigupReviewers.map((rev) => rev.Items[0].bitbucketId);
+    const resp = await jira.getUsersFromAccountIds(jiraAccountIds);
 
-    if (jiraUsers) {
+    if (resp && resp.values) {
       console.log(
         `Adding Jira Users as Code Reviewers: ${JSON.stringify(
-          jiraUsers.map(user => user.displayName)
+          resp.values.map((user) => user.displayName)
         )}`
       );
-      jira.addCodeReviewersToIssue(action.issue.key, jiraUsers);
+      jira.addCodeReviewersToIssue(action.issue.key, resp.values);
     }
 
-    core.setOutput("verified", `${valid}`);
+    core.setOutput('verified', `${valid}`);
   } catch (error) {
     core.setFailed(error.message);
   }
