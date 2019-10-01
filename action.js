e114d509d4d144173c48a70f1557c5dccf9aea74
duanebester <bester.duane@gmail.com>
2019-09-30 18:34:33 -0700

    update master

--- action.js
+++ action.js
@@ -146,14 +146,16 @@ module.exports = class {
 
   async updateCodeReviewers() {
     const reviewers = this.getCodeReviewers();
-    console.log(`Reviewers: ${JSON.stringify(reviewers)}`);
+    if (!reviewers || reviewers.length === 0) {
+      this.core.info('No Reviewers!');
+      return;
+    }
+
     const rigupReviewers = await Promise.all(
       reviewers.map(async (reviewer) => {
         return this.dynamo.findByGithubId(reviewer.id);
       })
     );
-
-    console.log(`Rigup Reviewers: ${JSON.stringify(rigupReviewers)}`);
 
     const jiraAccountIds = rigupReviewers.map((rev) => rev.Items[0].bitbucketId.S);
     const resp = await this.Jira.getUsersFromAccountIds(jiraAccountIds);
@@ -170,13 +172,17 @@ module.exports = class {
 
   async updateApprovers() {
     const approvers = this.getApprovers();
-    console.log(`Approvers: ${JSON.stringify(approvers)}`);
+
+    if (!approvers || approvers.length === 0) {
+      this.core.info('No Approvers!');
+      return;
+    }
+
     const rigupApprovers = await Promise.all(
       approvers.map(async (approver) => {
         return this.dynamo.findByGithubId(approver.id);
       })
     );
-    console.log(`Rigup Approvers: ${JSON.stringify(rigupApprovers)}`);
 
     const jiraAccountIds = rigupApprovers.map((rev) => rev.Items[0].bitbucketId.S);
     const resp = await this.Jira.getUsersFromAccountIds(jiraAccountIds);
